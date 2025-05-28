c9ba1594f7392c13cab8965ff9ebef3099f3c105
r33drichards <rwendt1337@gmail.com>
2025-05-28 09:37:44 -0700

    flake

--- shell.nix
+++ shell.nix
@@ -18,14 +18,5 @@ pkgs.mkShell {
     # tensordict
   ];
   shellHook = ''
-  export DYLD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.python3Packages.pytorch ]}
-  python3 -m venv .venv
-  # activate the virtual environment
-  source .venv/bin/activate
-  pip install 'gymnasium[atari]'
-  pip install 'gymnasium[accept-rom-license]'
-  pip install gym-super-mario-bros==7.4.0
-  pip install torchrl==0.3.0
-  pip install tensordict==0.3.0
   '';
 }
